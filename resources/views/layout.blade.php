36d8c9558b9eb5b9087cc9c0aa0f469de13844f4
juliouslara <larajulious@gmail.com>
2017-01-27 12:15:31 +0800

    npm installed

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Project Flyer</title>
+	<link rel="stylesheet" href="public/css/app.css">
+</head>
+<body>
+
+	<div class="container">
+		@yield('content')
+	</div>
+
+</body>
+</html>
